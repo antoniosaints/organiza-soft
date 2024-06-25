2a8ece57ea309200ecd5a8f9d4d822b7e73fe6f0
antoniosaints <costaantonio883@gmail.com>
2024-06-24 22:54:37 -0300

    atualizacao

--- api/src/controllers/bloqueio_controller.ts
+++ api/src/controllers/bloqueio_controller.ts
@@ -8,7 +8,7 @@ export const createBloqueio = async (req: any, res: any) => {
         const bloqueio = await prismaService.bloqueio.create({
             data: { assinaturaId, motivo, dataBloqueio, dataDesbloqueio }
         });
-        ResponseService.created(res, {message: "Bloqueio criado com sucesso", data: bloqueio});
+        ResponseService.created(res, { message: "Bloqueio criado com sucesso", data: bloqueio });
     } catch (error: any) {
         HttpErrorService.hadle(error, res);
     }
@@ -17,7 +17,7 @@ export const createBloqueio = async (req: any, res: any) => {
 export const getBloqueios = async (req: any, res: any) => {
     try {
         const bloqueios = await prismaService.bloqueio.findMany();
-        ResponseService.success(res, {data: bloqueios});
+        ResponseService.success(res, { data: bloqueios });
     } catch (error: any) {
         HttpErrorService.hadle(error, res);
     }
@@ -27,7 +27,7 @@ export const getBloqueio = async (req: any, res: any) => {
     const { id } = req.params;
     try {
         const bloqueio = await prismaService.bloqueio.findUnique({ where: { id: Number(id) } });
-        ResponseService.success(res, {data: bloqueio});
+        ResponseService.success(res, { data: bloqueio });
     } catch (error: any) {
         HttpErrorService.hadle(error, res);
     }
@@ -41,7 +41,7 @@ export const updateBloqueio = async (req: any, res: any) => {
             where: { id: Number(id) },
             data: { assinaturaId, motivo, dataBloqueio, dataDesbloqueio }
         });
-        ResponseService.success(res, {message: "Bloqueio atualizado com sucesso", data: bloqueio});
+        ResponseService.success(res, { message: "Bloqueio atualizado com sucesso", data: bloqueio });
     } catch (error: any) {
         HttpErrorService.hadle(error, res);
     }
@@ -51,7 +51,7 @@ export const deleteBloqueio = async (req: any, res: any) => {
     const { id } = req.params;
     try {
         await prismaService.bloqueio.delete({ where: { id: Number(id) } });
-        ResponseService.success(res, {message: "Bloqueio excluido com sucesso"});
+        ResponseService.success(res, { message: "Bloqueio excluido com sucesso" });
     } catch (error: any) {
         HttpErrorService.hadle(error, res);
     }
