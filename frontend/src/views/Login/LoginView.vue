3603c74e7a01df3b2a71438399f25e8242ee634b
antoniosaints <costaantonio883@gmail.com>
2024-07-23 09:59:27 -0300

    alterações

--- frontend/src/views/Login/LoginView.vue
+++ frontend/src/views/Login/LoginView.vue
@@ -1,42 +1,35 @@
 <template>
-  <div class="bg-gray-100 flex-col dark:bg-gray-900 w-screen p-4 h-screen flex justify-center items-center">
-    <div class="max-w-lg text-center">
-      <h1
-        class="mb-4 text-4xl font-extrabold leading-none tracking-tight text-gray-900 md:text-5xl lg:text-6xl dark:text-white">
-        Organiza
-        <span class="text-blue-600 dark:text-blue-500">SOFT</span>
-      </h1>
-      <p class="text-lg font-normal mb-10 text-gray-500 lg:text-xl dark:text-gray-400">
-        Gestão financeira, serviços, assinaturas e muito mais.
-      </p>
+  <div class="bg-gray-100 dark:bg-gray-900 w-screen p-0 md:p-4 lg:p-16 h-screen flex lg:justify-between justify-center items-center">
+    <div class="w-0 lg:w-[60%] lg:p-14">
+      <FwbImg src="logo-login.png" alt="logo" class="w-0 lg:w-[70%]" />
+    </div>
+    <div class="w-full lg:w-[40%] justify-center items-center flex flex-col h-screen md:h-auto md:max-w-md p-6 bg-white rounded-lg shadow dark:bg-gray-800">
+      <div class="max-w-lg text-center">
+        <h1
+          class="mb-4 text-4xl font-extrabold leading-none tracking-tight text-gray-900 md:text-5xl lg:text-6xl dark:text-white">
+          Organiza
+          <span class="text-blue-600 dark:text-blue-500">SOFT</span>
+        </h1>
+        <p class="text-lg font-normal mb-10 text-gray-500 lg:text-xl dark:text-gray-400">
+          Gestão financeira, serviços, assinaturas e muito mais.
+        </p>
+      </div>
+      <form class="w-full max-w-lg" @submit.prevent="loginUsuarioSistema">
+        <div class="mb-5">
+          <FwbInput v-model="user.email" required type="email" label="E-mail" placeholder="Seu e-mail" />
+        </div>
+        <div class="mb-5">
+          <FwbInput v-model="user.password" required type="password" label="Senha" placeholder="•••••••••" />
+        </div>
+        <div class="flex items-start mb-5">
+          <div class="flex items-center h-5">
+            <FwbCheckbox  v-model="rememberMe" label="Lembrar de mim" />
+          </div>
+        </div>
+        <FwbButton type="submit">Logar no sistema</FwbButton>
+      </form>
     </div>
 
-    <form class="w-full max-w-lg bg-slate-100 dark:bg-gray-900" @submit.prevent="auth">
-
-      <div class="mb-5">
-        <label for="text" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Usuário</label>
-        <input type="text" id="text" v-model="user.email"
-          class="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 dark:shadow-sm-light"
-          placeholder="Seu e-mail" required />
-      </div>
-      <div class="mb-5">
-        <label for="senha" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Senha</label>
-        <input type="senha" id="senha" placeholder="•••••••••" v-model="user.password"
-          class="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 dark:shadow-sm-light"
-          required />
-      </div>
-      <div class="flex items-start mb-5">
-        <div class="flex items-center h-5">
-          <input id="terms" type="checkbox" @click="toggleRememberMe" :checked="rememberMe"
-            class="w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-blue-300 dark:bg-gray-700 dark:border-gray-600 dark:focus:ring-blue-600 dark:ring-offset-gray-800 dark:focus:ring-offset-gray-800" />
-        </div>
-        <label for="terms" class="ms-2 text-sm font-medium text-gray-900 dark:text-gray-300">Lembrar de mim</label>
-      </div>
-      <button type="submit"
-        class="text-white w-full lg:w-auto md:w-auto bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
-        Entrar
-      </button>
-    </form>
   </div>
 </template>
 
@@ -45,6 +38,7 @@ import router from '@/routes/Router';
 import toast from '@/utils/toast';
 import { useLoginStore } from '@/stores/login';
 import { reactive, ref } from 'vue';
+import { FwbButton, FwbCheckbox, FwbImg, FwbInput } from 'flowbite-vue';
 
 const rememberMe = ref(JSON.parse(localStorage.getItem("rememberMe")) || false);
 const user = reactive({
@@ -52,10 +46,7 @@ const user = reactive({
   password: localStorage.getItem("@gestao_inteligente:password") || '',
 })
 
-const toggleRememberMe = () => {
-  rememberMe.value = !rememberMe.value
-}
-const auth = async () => {
+const loginUsuarioSistema = async () => {
   const logged = await useLoginStore().login(user);
   if (logged.success == true) {
     if (rememberMe.value == true) {
--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,11 +1,14 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}', './node_modules/flowbite/**/*.js'],
+  content: [
+    "index.html",
+    "src/**/*.{vue,js,ts,jsx,tsx}",
+    "node_modules/flowbite-vue/**/*.{js,jsx,ts,tsx,vue}",
+    "node_modules/flowbite/**/*.{js,jsx,ts,tsx}",
+  ],
   theme: {
     extend: {},
   },
-  darkMode: 'class',
-  plugins: [
-    require('flowbite/plugin')
-  ],
-}
+  darkMode: "class",
+  plugins: [require("flowbite/plugin")],
+};
