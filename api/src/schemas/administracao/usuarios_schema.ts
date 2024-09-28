d188a7eac67ceb5b019914676328c9e3b612a529
antoniosaints <costaantonio883@gmail.com>
2024-09-28 12:00:44 -0300

    feat(usuario) adicionado senha complexa para usuários

--- api/src/schemas/administracao/usuarios_schema.ts
+++ api/src/schemas/administracao/usuarios_schema.ts
@@ -23,6 +23,18 @@ const createUser = zodUtil.object({
     .min(8, {
       message: "A senha deve ter pelo menos 8 caracteres",
     })
+    .regex(/^(?=.*[a-z])/, {
+      message: "A senha deve conter pelo menos uma letra minúscula",
+    })
+    .regex(/^(?=.*[A-Z])/, {
+      message: "A senha deve conter pelo menos uma letra maiúscula",
+    })
+    .regex(/^(?=.*\d)/, {
+      message: "A senha deve conter pelo menos um número",
+    })
+    .regex(/^(?=.*[@$!%*?&])/, {
+      message: "A senha deve conter pelo menos um caractere especial",
+    })
     .trim(),
   telefone: zodUtil
     .string({
@@ -78,6 +90,18 @@ const updateUser = zodUtil.object({
     })
     .min(8, {
       message: "A senha deve ter pelo menos 8 caracteres",
+    })
+    .regex(/^(?=.*[a-z])/, {
+      message: "A senha deve conter pelo menos uma letra minúscula",
+    })
+    .regex(/^(?=.*[A-Z])/, {
+      message: "A senha deve conter pelo menos uma letra maiúscula",
+    })
+    .regex(/^(?=.*\d)/, {
+      message: "A senha deve conter pelo menos um número",
+    })
+    .regex(/^(?=.*[@$!%*?&.#])/, {
+      message: "A senha deve conter pelo menos um caractere especial (@$!%*?&.#)",
     })
     .trim()
     .optional(),
