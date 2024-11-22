a591a95e5a6fc528fc63eb61b5b1b66e716c9d52
Antonio Costa dos Santos <costaantonio883@gmail.com>
2024-11-22 09:46:23 -0300

    fic(bug) bugfix

--- vue/src/utils/storageUtil.ts
+++ vue/src/utils/storageUtil.ts
@@ -1,20 +1,13 @@
-import { ScToastUtil } from "./scToastUtil";
-
 export default class StorageUtil {
     static get(key: string): any {
-        if (!key || localStorage.getItem(key) === null) return ScToastUtil.info('Nenhuma informação encontrada com esse identificador');
-        const value = localStorage.getItem(key);
-        if (value) return JSON.parse(value);
+        return localStorage.getItem(key);
     }
 
     static set(key: string, value: any) {
-        if (!key || !value) return ScToastUtil.info('Informe Chave e Valor');
-        if (typeof value === 'object') value = JSON.stringify(value);
         localStorage.setItem(key, value);
     }
 
     static remove(key: string) {
-        if (!key) return ScToastUtil.info('Nenhuma informação encontrada com esse identificador');
         localStorage.removeItem(key);
     }
 }
