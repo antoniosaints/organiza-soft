6be8aee95e25a625581dcf78b0c722c19f544e0c
Antonio Costa dos Santos <costaantonio883@gmail.com>
2024-12-26 15:15:33 -0300

    feat(email) ajustes de email

--- api/app/services/email/email_service.ts
+++ api/app/services/email/email_service.ts
@@ -3,24 +3,53 @@ import env from '../../configs/env';
 import SMTPTransport from 'nodemailer/lib/smtp-transport';
 
 // Configuração do transporte
-const transporter = nodemailer.createTransport({
-    host: env.EMAIL_HOST, // Substitua pelo seu host SMTP
-    port: env.EMAIL_PORT, // Porta do servidor SMTP
-    secure: env.EMAIL_SECURE, // true para 465, false para outras portas
-    auth: {
-        user: env.EMAIL_USER, // Seu email
-        pass: env.EMAIL_PASSWORD, // Sua senha
+const createTransporter = () => {
+    const transporter = nodemailer.createTransport({
+        host: env.EMAIL_HOST,
+        port: env.EMAIL_PORT,
+        secure: env.EMAIL_SECURE,
+        auth: {
+            user: env.EMAIL_USER,
+            pass: env.EMAIL_PASSWORD,
+        }
+    });
+
+    return transporter;
+};
+
+// Função para verificar a configuração do email
+const verifyEmailConfig = async () => {
+    const transporter = createTransporter();
+    try {
+        await transporter.verify();
+        return true;
+    } catch (error) {
+        throw error;
     }
-});
+};
 
 // Função para enviar email
-export const sendEmail = async (to: string, subject: string, text: string, html?: string): Promise<SMTPTransport.SentMessageInfo> => {
+export const sendEmail = async (
+    to: string, 
+    subject: string, 
+    text: string, 
+    html?: string
+): Promise<SMTPTransport.SentMessageInfo> => {
+    // Verifica a configuração antes de tentar enviar
+    await verifyEmailConfig();
+
+    const transporter = createTransporter();
     const mailOptions = {
-        from: env.EMAIL_USER, // Seu email
+        from: env.EMAIL_USER,
         to,
         subject,
-        text,
-        html
+        text
     };
-    return await transporter.sendMail(mailOptions);
+
+    try {
+        const info = await transporter.sendMail(mailOptions);
+        return info;
+    } catch (error) {
+        throw error;
+    }
 };
